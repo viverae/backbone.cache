26c93d92aabf72a710f864b46572fc8afc813b22
Chris Miller <cmille142@gmail.com>
2012-10-12 12:18:34 -0700

    build

--- build/cache.js
+++ build/cache.js
@@ -1,4 +1,4 @@
-/*! or-cachejs - v0.1.0 - 2012-10-11 */
+/*! cachejs - v0.1.0 - 2012-10-12 */
 
 /*jshint*/
 /*global Backbone, define, console */
@@ -13,87 +13,18 @@
 }(this, function (Backbone, $, _) {
   "use strict";
 
-  var Encoders = {
-
-    base64 : {
-      /**
-       * Base64 Encode String
-       * Based Off Of Yahoo's Implementation
-       * @diff Replace("+", ".").Replace("/", "_").Replace("=", "-") <-- To Match With PHP Base64_encode
-       * @param string
-       */
-      encode : function (string) {
-        string = string || '';
-        var b64_keys = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789._-";
-        var output = "";
-        var n = 0;
-        var oct1, oct2, oct3;
-        var char1, char2, char3, char4;
-
-        while (n < string.length) {
-          oct1 = string.charCodeAt(n++);
-          oct2 = string.charCodeAt(n++);
-          oct3 = string.charCodeAt(n++);
-          char1 = oct1 >> 2;
-          char2 = ((oct1 & 3) << 4) | (oct2 >> 4);
-          char3 = ((oct2 & 15) << 2) | (oct3 >> 6);
-          char4 = oct3 & 63;
-
-          if (isNaN(oct2)) {
-            char3 = char4 = 64;
-          } else if (isNaN(oct3)) {
-            char4 = 64;
-          }
-          output = output + b64_keys.charAt(char1) + b64_keys.charAt(char2) + b64_keys.charAt(char3) + b64_keys.charAt(char4);
-        }
-        return output;
-      },
-
-      decode : function (string) {
-        string = string || '';
-        var b64_keys = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789._-";
-        var output = "";
-        var n = 0;
-        var oct1, oct2, oct3;
-        var char1, char2, char3, char4;
-
-        while (n < string.length) {
-          char1 = b64_keys.indexOf(string.charAt(n++));
-          char2 = b64_keys.indexOf(string.charAt(n++));
-          char3 = b64_keys.indexOf(string.charAt(n++));
-          char4 = b64_keys.indexOf(string.charAt(n++));
-          oct1 = (char1 << 2) | (char2 >> 4);
-          oct2 = ((char2 & 15) << 4) | (char3 >> 2);
-          oct3 = ((char3 & 3) << 6) | char4;
-
-          output = output + String.fromCharCode(oct1);
-          if (char3 !== 64) {
-            output = output + String.fromCharCode(oct2);
-          }
-          if (char4 !== 64) {
-            output = output + String.fromCharCode(oct3);
-          }
-        }
-        return output;
-      }
-    }
-  };
-
   var Cache = {
 
     cache : new Backbone.Model(),
 
     store : new $.store(),
 
-    encoders : {base64 : Encoders.base64},
-
-    encoder : 'base64',
-
     scrambled : false,
 
-    setEncoder : function(key) {
-      //Allow Default base64 Encoding
-      if (this.encoders[key]) this.encoder = key;
+    encoder : null,
+
+    setEncoder : function(encoder) {
+      if (_.isObject(encoder)) this.encoder = encoder;
     },
 
     setScramble : function(bool) {
@@ -101,11 +32,19 @@
     },
 
     scramble : function(string) {
-      return this.encoders[this.encoder].encode(string);
+      if (!this.encoder || (this.encoder && !_.isFunction(this.encoder.encode))) {
+        throw new Error("Cache Encoder Not Set");
+      } else {
+        return this.encoder.encode(string);
+      }
     },
 
     unscramble : function(string) {
-      return this.encoders[this.encoder].decode(string);
+      if (!this.encoder || (this.encoder && !_.isFunction(this.encoder.decode))) {
+        throw new Error("Cache Decoder Not Set");
+      } else {
+        return this.encoder.decode(string);
+      }
     },
 
     get : function(key, forceNew) {
